e76010c74b13e68e7fc697043c274b44781f7e22
Sivapriyadarsini.A <sivapriyadarsiniannamalai@gmail.com>
2025-03-03 13:22:59 +0530

    Update Stringmenuoperations.java

--- Stringmenuoperations.java
+++ Stringmenuoperations.java
@@ -33,6 +33,18 @@ class StringMenu {
         System.out.println("After Concatenation: " + str);
     }
 
+    // Compare the stored string with another user-input string
+    public void compareString(Scanner sc) {
+        System.out.print("Enter another string to compare: ");
+        String anotherString = sc.nextLine();
+
+        if (str.equals(anotherString)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are NOT equal.");
+        }
+    }
+
     public static void main(String[] args) {
         StringMenu obj = new StringMenu();
         Scanner sc = new Scanner(System.in);
@@ -43,7 +55,8 @@ class StringMenu {
             System.out.println("3) Copy String");
             System.out.println("4) Find Length of String");
             System.out.println("5) Concatenate String");
-            System.out.println("6) Exit");
+            System.out.println("6) Compare String");
+            System.out.println("7) Exit");
 
             System.out.print("Choose an option: ");
             int option = sc.nextInt();
@@ -66,6 +79,9 @@ class StringMenu {
                     obj.concatenate(sc);
                     break;
                 case 6:
+                    obj.compareString(sc);
+                    break;
+                case 7:
                     System.out.println("Exiting...");
                     sc.close();
                     return;
